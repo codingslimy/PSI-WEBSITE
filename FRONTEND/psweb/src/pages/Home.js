4f51b19e8dda9426f7a5a86a32cd20e50da3352e
Krishna Adhikari <krishnaadhikari0213@gmail.com>
2024-02-28 20:28:35 +0530

    Code

--- FRONTEND/psweb/src/pages/Home.js
+++ FRONTEND/psweb/src/pages/Home.js
@@ -5,32 +5,10 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { Link } from 'react-router-dom';
 import '../Style/Home.css';
-
-
-
-
-
-const NavBar = () => (
-  <nav className='nav'>
-        <div className='logo'>
-      <img
-        alt="Logo"
-        src="https://cdn.discordapp.com/attachments/1205756212445192263/1207363674990313562/Dr.Palamsetty_Innovations123.png?ex=65df600b&is=65cceb0b&hm=d8811294349ee35313077b961c60da34b5a054821e87fd435d7ed27f448f3c38&" 
-        style={{ height: '30px', width: '100px' }}  
-      />
-    </div>
-    <ul className='navUl'>
-      <li className= 'navLi'><Link to="/">Home</Link></li>
-      <li className='navLi'><Link to="/programmes">Our Programmes</Link></li>
-      <li className='navLi'><Link to="/clubs">Clubs</Link></li>
-      <li className='navLi'><Link to="/about">About Us</Link></li>
-      <li className='navLi'><Link to="/contact">Contact Us</Link></li>
-    </ul>
-    <Link to ="/login"><div className='loginButton'>Login</div></Link>
-  </nav>
-);
+import NavBar from '../components/NavBar';
+import Footer from '../components/Footer';
+import '../Style/scrollbar.css'
 
 const ImageSlider = () => (
   <div className='sliderContainer'>
@@ -98,18 +76,19 @@ const ProgramGrid = () => (
   </div>
 );
 
-const ClubCard = () => {
-  
+const ClubCard = ({ imageSrc, clubName }) => {
   return (
-    <div className="card" >
+    <div className="card">
       <div className="card-details">
-        <p className="text-title">Card title</p>
+        <img src={imageSrc} alt={clubName} />
+        <p className="text-title">{clubName}</p>
         <p className="text-body">Here are the details of the card</p>
       </div>
       <button className="card-button">Join Now</button>
     </div>
   );
-}
+};
+
 const MainContent = () => (
   <main className='main'>
     <ImageSlider />
@@ -140,7 +119,7 @@ const MainContent = () => (
     
 
     <h1>OUR CLUBS</h1>
-     <div className="clubCard" style={{flexDirection:'row',display:'flex',margin:'20px',
+    <div className="clubCard" style={{ display: 'flex', justifyContent: 'space-around', flexWrap: 'wrap' ,
      backgroundImage: 'url(https://media.discordapp.net/attachments/1205756212445192263/1209453048263479348/club_background.png?ex=65e6f9ed&is=65d484ed&hm=62024ebbcb2b02ba5a8a0d863ad1cafd339cd7ff775e26a6258a1e18e062cebd&=&format=webp&quality=lossless&width=1322&height=662)',
      backgroundSize: 'cover',
      backgroundRepeat: 'no-repeat',
@@ -198,42 +177,6 @@ const StatisticsSection = () => (
   </div>
 );
 
-
-const Footer = () => (
-  
-  <footer className="footer"style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between', padding: '30px' }}>
-    
-    <div style={{ marginRight: 'auto', textAlign: 'left' }}>
-      <h2 style={{ marginBottom: '5px' }}>Contact Us</h2>
-      <ul style={{ listStyle: 'none', padding: 0 }}>
-        <li>• Company Name: ABC Corporation</li>
-        <li>• Email: info@abccorp.com</li>
-        <li>• Phone: (555) 123-4567</li>
-        <li>• Address: 123 Main Street, Cityville, State, 12345</li>
-      </ul>
-    </div>
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginRight: 'auto', textAlign: 'left'}}>
-      
-      <h2 style={{ marginBottom: '10px' }}>Menu</h2>
-      <ul style={{ listStyle: 'none', padding: 0 }}>
-        <li>• Home</li>
-        <li>• About Us</li>
-        <li>• Partners</li>
-        <li>• Clubs</li>
-        <li>• Contact Us</li>
-      </ul>
-    </div>
-    <div style={{ textAlign: 'center' ,display: 'flex', flexDirection: 'column', alignItems: 'left' }}>
-      <div style={{ width: '50px', height: '50px', borderRadius: '50%', backgroundColor: '#ccc', marginBottom: '10px' }}></div>
-      <p>Follow us on</p>
-      <div style={{ display: 'flex' }}>
-        <div style={{ width: '20px', height: '20px', borderRadius: '50%', backgroundColor: '#ccc', marginRight: '5px' }}></div>
-        <div style={{ width: '20px', height: '20px', borderRadius: '50%', backgroundColor: '#ccc', marginRight: '5px' }}></div>
-        <div style={{ width: '20px', height: '20px', borderRadius: '50%', backgroundColor: '#ccc', marginRight: '5px' }}></div>
-      </div>
-    </div>
-  </footer>
-);
 
 const Home = () => (
   <div className="app">
--- FRONTEND/psweb/src/pages/contactus.js
+++ FRONTEND/psweb/src/pages/contactus.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import "../Style/contactus.css";
-import { Link } from 'react-router-dom';
+import NavBar from '../components/NavBar';
+import Footer from '../components/Footer';
+
 
 const ContactUs = () => {
   const handleFileInputChange = (event) => {
@@ -11,23 +13,7 @@ const ContactUs = () => {
 
   return (
     <div className="appContact">
-      <nav className='nav'>
-        <div className='logo'>
-          <img
-            alt="Logo"
-            src="https://cdn.discordapp.com/attachments/1205756212445192263/1207363674990313562/Dr.Palamsetty_Innovations123.png?ex=65df600b&is=65cceb0b&hm=d8811294349ee35313077b961c60da34b5a054821e87fd435d7ed27f448f3c38&" 
-            style={{ height: '30px', width: '100px' }}  
-          />
-        </div>
-        <ul className='navUl'>
-          <li className= 'navLi'><Link to="/">Home</Link></li>
-          <li className='navLi'><Link to="/programmes">Our Programmes</Link></li>
-          <li className='navLi'><Link to="/clubs">Clubs</Link></li>
-          <li className='navLi'><Link to="/about">About Us</Link></li>
-          <li className='navLi'><Link to="/contact">Contact Us</Link></li>
-        </ul>
-        <div className='loginButton'>Login</div>
-      </nav>
+      <NavBar/>
       <div className='pane'>
         <div className="verticalBox">
           <img
@@ -67,36 +53,7 @@ const ContactUs = () => {
           </div>
         </div>
       </div>
-      <footer className="footer" style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', padding: '30px' }}>
-        <div style={{ marginRight: 'auto', textAlign: 'left' }}>
-          <h2 style={{ marginBottom: '5px' }}>Contact Us</h2>
-          <ul style={{ listStyle: 'none', padding: 0 }}>
-            <li>• Company Name: ABC Corporation</li>
-            <li>• Email: info@abccorp.com</li>
-            <li>• Phone: (555) 123-4567</li>
-            <li>• Address: 123 Main Street, Cityville, State, 12345</li>
-          </ul>
-        </div>
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginRight: 'auto', textAlign: 'left' }}>
-          <h2 style={{ marginBottom: '10px' }}>Menu</h2>
-          <ul style={{ listStyle: 'none', padding: 0 }}>
-            <li>• Home</li>
-            <li>• About Us</li>
-            <li>• Partners</li>
-            <li>• Clubs</li>
-            <li>• Contact Us</li>
-          </ul>
-        </div>
-        <div style={{ textAlign: 'center', display: 'flex', flexDirection: 'column', alignItems: 'left' }}>
-          <div style={{ width: '50px', height: '50px', borderRadius: '50%', backgroundColor: '#ccc', marginBottom: '10px' }}></div>
-          <p>Follow us on</p>
-          <div style={{ display: 'flex' }}>
-            <div style={{ width: '20px', height: '20px', borderRadius: '50%', backgroundColor: '#ccc', marginRight: '5px' }}></div>
-            <div style={{ width: '20px', height: '20px', borderRadius: '50%', backgroundColor: '#ccc', marginRight: '5px' }}></div>
-            <div style={{ width: '20px', height: '20px', borderRadius: '50%', backgroundColor: '#ccc', marginRight: '5px' }}></div>
-          </div>
-        </div>
-      </footer>
+      <Footer/>
     </div>
   );
 };
